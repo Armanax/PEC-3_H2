2fb3c64b778e913ebb0bcd9ad63cd80bfc106652
Armanax <arkantiz_95@outlook.com>
2023-06-02 12:21:00 -0500

    00

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/index.html', './src/second.html', './src/views/footer.html', './src/views/header.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
